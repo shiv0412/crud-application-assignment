7d646b596f2ffd8cc3c949fbc69338b35e52aa55
shiv0412 <shivomthakur38@gmail.com>
2022-10-10 15:21:18 +0530

    some-code-pushed

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -22,4 +22,6 @@ export const toastErrorNotification = (message) =>
     progress: undefined,
   });
 
-export const defaultQuanity = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+export const defaultQuanity = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+];
