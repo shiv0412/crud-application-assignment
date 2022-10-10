b246e1bc5dd7bdf1ba96c22b79a2594c2b06c668
Shivom <shivomthakur38@gmail.com>
2022-10-11 03:17:12 +0530

    some-code-pushed

--- frontend/src/redux/actions/Actions.js
+++ frontend/src/redux/actions/Actions.js
@@ -4,35 +4,30 @@ export const saveCustomersDetailsToStore = (values) => {
     values,
   };
 };
-
 export const saveProductsDetailsToStore = (values) => {
   return {
     type: "SAVEPRODUCTS",
     values,
   };
 };
-
 export const saveInvoicesDetailsToStore = (values) => {
   return {
     type: "SAVEINVOICES",
     values,
   };
 };
-
 export const setPurchasedProducts = (values) => {
   return {
     type: "SAVEPURCHASEDPRODUCT",
     values,
   };
 };
-
 export const setGeneratedInvoiceDetails = (values) => {
   return {
     type: "SAVEGENERATEDINVOICE",
     values,
   };
 };
-
 export const updateStore = () => {
   return {
     type: "UPDATESTORE",
