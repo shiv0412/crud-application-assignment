e6caf685851b2d7e6c344b5b370429ab26311466
shiv0412 <shivomthakur38@gmail.com>
2022-10-10 12:38:30 +0530

    pushed-some-code

--- frontend/src/components/invoices/InvoiceEdit.js
+++ frontend/src/components/invoices/InvoiceEdit.js
@@ -4,6 +4,7 @@ import { Field, Form, Formik } from "formik";
 import * as yup from "yup";
 import { connect } from "react-redux";
 import axios from "axios";
+import uniqueRandom from "unique-random";
 
 import CustomErrorMessage from "../shared/CustomerErrorMessage";
 import { SelectField } from "../shared/CustomSelectField";
@@ -32,9 +33,9 @@ const ModalContainer = styled.div`
   justify-content: center;
   h4 {
     text-align: center;
-    border-bottom: 1px solid #CACFD2;
+    border-bottom: 1px solid #cacfd2;
     padding-bottom: 10px;
-    color:#566573;
+    color: #566573;
   }
 `;
 const ModalContent = styled.div`
@@ -99,9 +100,14 @@ const InvoiceEdit = ({
   setGeneratedInvoiceDetails,
 }) => {
   const [isInvoiceGenerated, setIsInvoiceGenerated] = React.useState(false);
+  const [isProductAddedToInvoice, setIsProductAddedToInvoice] =
+    React.useState(false);
+  const randomNumber = uniqueRandom(11000, 20000); //just for test not a real use case
+
   if (!isEditing) {
     return null;
   }
+
   const getCustomersOptions = (customerData) => {
     const options = [];
     customerData.forEach((customer) => {
@@ -129,6 +135,7 @@ const InvoiceEdit = ({
           parseInt(addedProduct[0].unitPrice) *
           parseInt(values.productQuantity),
       });
+      setIsProductAddedToInvoice(false);
     }
   };
 
@@ -154,6 +161,7 @@ const InvoiceEdit = ({
   };
 
   const handleReceiptClosed = () => {
+    updateStore();
     handleFormClose();
     setIsInvoiceGenerated(false);
   };
@@ -184,18 +192,27 @@ const InvoiceEdit = ({
                   productData={productData}
                   handleProductAdd={handleProductAdd}
                 />
+                {isProductAddedToInvoice ? (
+                  <span>No product added please add atleast one product.</span>
+                ) : (
+                  ""
+                )}
                 <h4>Add Billing Details</h4>
                 <Formik
                   validationSchema={validationSchema}
                   initialValues={{
-                    invoiceNumber: "",
+                    invoiceNumber: randomNumber(),
                     invoiceDate: "",
                     customerName: "",
                   }}
                   onSubmit={(values, { setSubmitting }) => {
                     setTimeout(() => {
-                      handleGenerateInvoice(values, purchasedProducts);
-                      setSubmitting(false);
+                      if (purchasedProducts && purchasedProducts.length > 0) {
+                        handleGenerateInvoice(values, purchasedProducts);
+                        setSubmitting(false);
+                      } else {
+                        setIsProductAddedToInvoice(true);
+                      }
                     }, 400);
                   }}
                 >
@@ -213,13 +230,15 @@ const InvoiceEdit = ({
                       options={getCustomersOptions(customerData)}
                       placeholder={"Select customer..."}
                     />
-                    <CustomErrorMessage name="customerName"></CustomErrorMessage>
-                    <CustomErrorMessage name="invoiceDate"></CustomErrorMessage>
+
                     <StyledField
                       type="date"
                       name="invoiceDate"
                       placeholder="invoice date"
                     />
+                    <CustomErrorMessage name="customerName"></CustomErrorMessage>
+                    <br />
+                    <CustomErrorMessage name="invoiceDate"></CustomErrorMessage>
                     <Button type="submit">Generate Invoice</Button>
                   </Form>
                 </Formik>
